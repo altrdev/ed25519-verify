8e38f7a6a12409cd0826bc132376aeed1285f041
Alessandro Travi <alessandro.travi@xpeppers.com>
2021-04-07 22:23:56 +0200

    [fix]: github action deploy command

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,6 @@ import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
 import nodePolyfills from 'rollup-plugin-node-polyfills';
-import * as buffer from "buffer";
 
 const production = !process.env.ROLLUP_WATCH;
 
