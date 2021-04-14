91a23b20b90605a713015e9763ff6e8c169feafe
Alessandro Travi <alessandro.travi@xpeppers.com>
2021-04-14 17:39:06 +0200

    [feat]: added local storage dark mode

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
 	import FormVerify from './FormVerify.svelte';
 	import { MaterialApp, AppBar, Icon, Button } from 'svelte-materialify';
-	import { mdiInvertColors } from '@mdi/js';
-	import { theme } from './store.js';
+
+	import { theme, themeIcon } from './store.js';
+	import { mdiWeatherNight, mdiWeatherSunny } from "@mdi/js";
+
+	let themeIconToggled = themeIcon;
 
 	function toggleTheme() {
-		if ($theme === 'light') theme.set('dark');
-		else theme.set('light');
+		if ($theme === 'light') {
+			theme.set('dark');
+			themeIconToggled = mdiWeatherSunny;
+		}
+		else {
+			theme.set('light');
+			themeIconToggled = mdiWeatherNight;
+		}
 	}
 </script>
 
@@ -15,7 +24,7 @@
 		<span slot="title">Ed25519 Verify Signature</span>
 		<div style="flex-grow:1"></div>
 		<Button icon on:click={toggleTheme}>
-			<Icon path={mdiInvertColors}/>
+			<Icon path={themeIconToggled}/>
 		</Button>
 	</AppBar>
 	<FormVerify message="" signature="" publicKey=""/>
--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,10 @@
 import { writable } from "svelte/store";
+import { mdiWeatherNight, mdiWeatherSunny } from '@mdi/js';
 
 const storedTheme = localStorage.getItem("theme");
 export const theme = writable(storedTheme);
+
+export const themeIcon = storedTheme === 'dark' ? mdiWeatherSunny : mdiWeatherNight;
 
 theme.subscribe(value => {
     localStorage.setItem("theme", value === 'dark' ? 'dark' : 'light');
